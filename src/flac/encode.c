f4035ba7f529c137150f1bfdadff9d9a18a8947b
Josh Coalson <jcoalson@users.sourceforce.net>
2001-04-24 22:42:46 +0000

    remove the code that printed out the seek points before encoding

--- src/flac/encode.c
+++ src/flac/encode.c
@@ -554,20 +554,6 @@ bool init_encoder(bool lax, bool do_mid_side, bool loose_mid_side, bool do_exhau
 		return false;
 	}
 
-	if(encoder_wrapper->verbose && encoder_wrapper->seek_table.num_points > 0) {
-		if(encoder_wrapper->total_samples_to_encode > 0)
-			printf("seek points (%llu total samples):", encoder_wrapper->total_samples_to_encode);
-		else
-			printf("seek points (total samples unknown):");
-		for(i = 0; i < encoder_wrapper->seek_table.num_points; i++) {
-			if(encoder_wrapper->seek_table.points[i].sample_number != FLAC__STREAM_METADATA_SEEKPOINT_PLACEHOLDER)
-				printf(" %llu", encoder_wrapper->seek_table.points[i].sample_number);
-			else
-				printf(" X");
-		}
-		printf("\n");
-	}
-
 	encoder_wrapper->encoder->streamable_subset = !lax;
 	encoder_wrapper->encoder->channels = channels;
 	encoder_wrapper->encoder->bits_per_sample = bps;
