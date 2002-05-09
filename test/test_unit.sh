cdcf19a122b30d07ba37f923631522da0a31e6e8
Josh Coalson <jcoalson@users.sourceforce.net>
2002-05-09 05:53:46 +0000

    use full path to test_unit

--- test/test_unit.sh
+++ test/test_unit.sh
@@ -21,7 +21,7 @@ LD_LIBRARY_PATH=../src/libFLAC/.libs:../obj/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
 PATH=../src/test_unit:../obj/bin:$PATH
 
-if test_unit ; then : ; else
-	echo "ERROR during testgen" 1>&2
+if `which test_unit` ; then : ; else
+	echo "ERROR during test_unit" 1>&2
 	exit 1
 fi
