e3c6a4c2055a6394ebf8e411b6cb891167fcd85c
Josh Coalson <jcoalson@users.sourceforce.net>
2002-05-09 05:27:17 +0000

    add FLAC__ASSERT_DECLARATION macro

--- include/FLAC/assert.h
+++ include/FLAC/assert.h
@@ -24,8 +24,10 @@
 #ifdef DEBUG
 #include <assert.h>
 #define FLAC__ASSERT(x) assert(x)
+#define FLAC__ASSERT_DECLARATION(x) x
 #else
 #define FLAC__ASSERT(x)
+#define FLAC__ASSERT_DECLARATION(x)
 #endif
 
 #endif
