b5988941cc2762af8b771771864882f759b0330d
Josh Coalson <jcoalson@users.sourceforce.net>
2002-06-07 06:10:08 +0000

    rename test_unit.sh to test_libFLAC.sh

---
+++ test/test_libFLAC.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#  FLAC - Free Lossless Audio Codec
+#  Copyright (C) 2001,2002  Josh Coalson
+#
+#  This program is part of FLAC; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+LD_LIBRARY_PATH=../src/libFLAC/.libs:../obj/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+PATH=../src/test_libFLAC:../obj/bin:$PATH
+export PATH
+
+if test_libFLAC ; then : ; else
+	echo "ERROR during test_libFLAC" 1>&2
+	exit 1
+fi
