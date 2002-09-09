b4bc14ba2f5db0c267ce2d03ca5c3f29380845a9
uid38180 <uid38180>
2002-09-09 20:53:08 +0000

    unwind gettext usage; see tag COMMENT_LAST_WORKING_GETTEXT for the last working version of the gettext flow

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gettextize --intl && aclocal && autoconf && autoheader && automake --foreign --include-deps --add-missing --copy
+aclocal && autoconf && autoheader && automake --foreign --include-deps --add-missing --copy
