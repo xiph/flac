f12edc643bb8a662384b0828b1b0504d5a4ef118
Josh Coalson <jcoalson@users.sourceforce.net>
2002-10-11 06:24:33 +0000

    fix assertion bug

--- src/libFLAC/format.c
+++ src/libFLAC/format.c
@@ -243,7 +243,10 @@ unsigned FLAC__format_get_max_rice_partition_order_from_blocksize_limited_max_an
 	while(max_rice_partition_order > 0 && (blocksize >> max_rice_partition_order) <= predictor_order)
 		max_rice_partition_order--;
 
-	FLAC__ASSERT(blocksize >> max_rice_partition_order > predictor_order);
+	FLAC__ASSERT(
+		(max_rice_partition_order == 0 && blocksize >= predictor_order) ||
+		(max_rice_partition_order > 0 && blocksize >> max_rice_partition_order > predictor_order)
+	);
 
 	return max_rice_partition_order;
 }
