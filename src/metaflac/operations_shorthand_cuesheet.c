xiph/flac

commit ce8a75134cace056f6c436d54b57bad1a1d93797

Fix a bunch of printf format warnings.

src/metaflac/operations_shorthand_cuesheet.c
@@ -162,11 +162,7 @@ FLAC__bool import_cs_from(const char *filename, FLAC__StreamMetadata **cuesheet,
 		for(track = 0; track < cs->num_tracks; track++) {
 			const FLAC__StreamMetadata_CueSheet_Track *tr = cs->tracks+track;
 			for(index = 0; index < tr->num_indices; index++) {
-#ifdef _MSC_VER
-				sprintf(spec, "%I64u;", tr->offset + tr->indices[index].offset);
-#else
-				sprintf(spec, "%llu;", (unsigned long long)(tr->offset + tr->indices[index].offset));
-#endif
+				sprintf(spec, "%" PRIu64 "u;", (uint64_t)(tr->offset + tr->indices[index].offset));
 				local_strcat(seekpoint_specification, spec);
 			}
 		}

examples/c/decode/file/main.c
@@ -174,11 +174,7 @@ void metadata_callback(const FLAC__StreamDecoder *decoder, const FLAC__StreamMet
 		fprintf(stderr, "sample rate    : %u Hz\n", sample_rate);
 		fprintf(stderr, "channels       : %u\n", channels);
 		fprintf(stderr, "bits per sample: %u\n", bps);
-#ifdef _MSC_VER
-		fprintf(stderr, "total samples  : %I64u\n", total_samples);
-#else
-		fprintf(stderr, "total samples  : %llu\n", total_samples);
-#endif
+		fprintf(stderr, "total samples  : %" PRIu64 "\n", total_samples);
 	}
 }
 

examples/c/encode/file/main.c
@@ -79,7 +79,7 @@ int main(int argc, char *argv[])
 	channels = 2;
 	bps = 16;
 	total_samples = (((((((unsigned)buffer[43] << 8) | buffer[42]) << 8) | buffer[41]) << 8) | buffer[40]) / 4;
-   
+
 	/* allocate the encoder */
 	if((encoder = FLAC__stream_encoder_new()) == NULL) {
 		fprintf(stderr, "ERROR: allocating encoder\n");
@@ -165,9 +165,5 @@ void progress_callback(const FLAC__StreamEncoder *encoder, FLAC__uint64 bytes_wr
 {
 	(void)encoder, (void)client_data;
 
-#ifdef _MSC_VER
-	fprintf(stderr, "wrote %I64u bytes, %I64u/%u samples, %u/%u frames\n", bytes_written, samples_written, total_samples, frames_written, total_frames_estimate);
-#else
-	fprintf(stderr, "wrote %llu bytes, %llu/%u samples, %u/%u frames\n", bytes_written, samples_written, total_samples, frames_written, total_frames_estimate);
-#endif
+	fprintf(stderr, "wrote %" PRIu64 " bytes, %" PRIu64 "/%u samples, %u/%u frames\n", bytes_written, samples_written, total_samples, frames_written, total_frames_estimate);
 }

src/flac/analyze.c
@@ -25,6 +25,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 #include "FLAC/all.h"
 #include "analyze.h"
 
@@ -66,11 +67,7 @@ void flac__analyze_frame(const FLAC__Frame *frame, unsigned frame_number, FLAC__
 	unsigned i, channel, partitions;
 
 	/* do the human-readable part first */
-#ifdef _MSC_VER
-	fprintf(fout, "frame=%u\toffset=%I64u\tbits=%u\tblocksize=%u\tsample_rate=%u\tchannels=%u\tchannel_assignment=%s\n", frame_number, frame_offset, frame_bytes*8, frame->header.blocksize, frame->header.sample_rate, channels, FLAC__ChannelAssignmentString[frame->header.channel_assignment]);
-#else
-	fprintf(fout, "frame=%u\toffset=%llu\tbits=%u\tblocksize=%u\tsample_rate=%u\tchannels=%u\tchannel_assignment=%s\n", frame_number, (unsigned long long)frame_offset, frame_bytes*8, frame->header.blocksize, frame->header.sample_rate, channels, FLAC__ChannelAssignmentString[frame->header.channel_assignment]);
-#endif
+	fprintf(fout, "frame=%u\toffset=%" PRIu64 "\tbits=%u\tblocksize=%u\tsample_rate=%u\tchannels=%u\tchannel_assignment=%s\n", frame_number, (uint64_t)frame_offset, frame_bytes*8, frame->header.blocksize, frame->header.sample_rate, channels, FLAC__ChannelAssignmentString[frame->header.channel_assignment]);
 	for(channel = 0; channel < channels; channel++) {
 		const FLAC__Subframe *subframe = frame->subframes+channel;
 		const FLAC__bool is_rice2 = subframe->data.fixed.entropy_coding_method.type == FLAC__ENTROPY_CODING_METHOD_PARTITIONED_RICE2;

src/flac/iffscan.c
@@ -114,15 +114,9 @@ int main(int argc, char *argv[])
 				fprintf(stderr, "ERROR reading %s\n", fn);
 				return 1;
 			}
-#ifdef _MSC_VER
-			printf(" RIFF size=%016I64x=(%I64u)", unpack64le_(buf+8), unpack64le_(buf+8));
-			printf(" data size=%016I64x=(%I64u)", unpack64le_(buf+16), unpack64le_(buf+16));
-			printf(" sample count=%016I64x=(%I64u)", unpack64le_(buf+24), unpack64le_(buf+24));
-#else
-			printf(" RIFF size=%016llx=(%llu)", unpack64le_(buf+8), unpack64le_(buf+8));
-			printf(" data size=%016llx=(%llu)", unpack64le_(buf+16), unpack64le_(buf+16));
-			printf(" sample count=%016llx=(%llu)", unpack64le_(buf+24), unpack64le_(buf+24));
-#endif
+			printf(" RIFF size=%016" PRIx64 "=(" PRIu64 ")", unpack64le_(buf+8), unpack64le_(buf+8));
+			printf(" data size=%016" PRIx64 "=(" PRIu64 ")", unpack64le_(buf+16), unpack64le_(buf+16));
+			printf(" sample count=%016" PRIx64 "=(" PRIu64 ")", unpack64le_(buf+24), unpack64le_(buf+24));
 			printf(" table size=%08x=(%u)", unpack32le_(buf+32), unpack32le_(buf+32));
 		}
 		printf("\n");

src/metaflac/operations.c
@@ -565,11 +565,7 @@ void write_metadata(const char *filename, FLAC__StreamMetadata *block, unsigned 
 			PPR; printf("  sample_rate: %u Hz\n", block->data.stream_info.sample_rate);
 			PPR; printf("  channels: %u\n", block->data.stream_info.channels);
 			PPR; printf("  bits-per-sample: %u\n", block->data.stream_info.bits_per_sample);
-#ifdef _MSC_VER
-			PPR; printf("  total samples: %I64u\n", block->data.stream_info.total_samples);
-#else
-			PPR; printf("  total samples: %llu\n", (unsigned long long)block->data.stream_info.total_samples);
-#endif
+			PPR; printf("  total samples: %" PRIu64 "\n", (uint64_t)block->data.stream_info.total_samples);
 			PPR; printf("  MD5 signature: ");
 			for(i = 0; i < 16; i++) {
 				printf("%02x", (unsigned)block->data.stream_info.md5sum[i]);
@@ -596,11 +592,7 @@ void write_metadata(const char *filename, FLAC__StreamMetadata *block, unsigned 
 			PPR; printf("  seek points: %u\n", block->data.seek_table.num_points);
 			for(i = 0; i < block->data.seek_table.num_points; i++) {
 				if(block->data.seek_table.points[i].sample_number != FLAC__STREAM_METADATA_SEEKPOINT_PLACEHOLDER) {
-#ifdef _MSC_VER
-					PPR; printf("    point %u: sample_number=%I64u, stream_offset=%I64u, frame_samples=%u\n", i, block->data.seek_table.points[i].sample_number, block->data.seek_table.points[i].stream_offset, block->data.seek_table.points[i].frame_samples);
-#else
-					PPR; printf("    point %u: sample_number=%llu, stream_offset=%llu, frame_samples=%u\n", i, (unsigned long long)block->data.seek_table.points[i].sample_number, (unsigned long long)block->data.seek_table.points[i].stream_offset, block->data.seek_table.points[i].frame_samples);
-#endif
+					PPR; printf("    point %u: sample_number=%" PRIu64 ", stream_offset=%" PRIu64 ", frame_samples=%u\n", i, (uint64_t)block->data.seek_table.points[i].sample_number, (uint64_t)block->data.seek_table.points[i].stream_offset, block->data.seek_table.points[i].frame_samples);
 				}
 				else {
 					PPR; printf("    point %u: PLACEHOLDER\n", i);
@@ -618,11 +610,7 @@ void write_metadata(const char *filename, FLAC__StreamMetadata *block, unsigned 
 			break;
 		case FLAC__METADATA_TYPE_CUESHEET:
 			PPR; printf("  media catalog number: %s\n", block->data.cue_sheet.media_catalog_number);
-#ifdef _MSC_VER
-			PPR; printf("  lead-in: %I64u\n", block->data.cue_sheet.lead_in);
-#else
-			PPR; printf("  lead-in: %llu\n", (unsigned long long)block->data.cue_sheet.lead_in);
-#endif
+			PPR; printf("  lead-in: %" PRIu64 "\n", (uint64_t)block->data.cue_sheet.lead_in);
 			PPR; printf("  is CD: %s\n", block->data.cue_sheet.is_cd? "true":"false");
 			PPR; printf("  number of tracks: %u\n", block->data.cue_sheet.num_tracks);
 			for(i = 0; i < block->data.cue_sheet.num_tracks; i++) {
@@ -630,11 +618,7 @@ void write_metadata(const char *filename, FLAC__StreamMetadata *block, unsigned 
 				const FLAC__bool is_last = (i == block->data.cue_sheet.num_tracks-1);
 				const FLAC__bool is_leadout = is_last && track->num_indices == 0;
 				PPR; printf("    track[%u]\n", i);
-#ifdef _MSC_VER
-				PPR; printf("      offset: %I64u\n", track->offset);
-#else
-				PPR; printf("      offset: %llu\n", (unsigned long long)track->offset);
-#endif
+				PPR; printf("      offset: %" PRIu64 "\n", (uint64_t)track->offset);
 				if(is_last) {
 					PPR; printf("      number: %u (%s)\n", (unsigned)track->number, is_leadout? "LEAD-OUT" : "INVALID");
 				}
@@ -649,11 +633,7 @@ void write_metadata(const char *filename, FLAC__StreamMetadata *block, unsigned 
 					for(j = 0; j < track->num_indices; j++) {
 						const FLAC__StreamMetadata_CueSheet_Index *index = track->indices+j;
 						PPR; printf("        index[%u]\n", j);
-#ifdef _MSC_VER
-						PPR; printf("          offset: %I64u\n", index->offset);
-#else
-						PPR; printf("          offset: %llu\n", (unsigned long long)index->offset);
-#endif
+						PPR; printf("          offset: %" PRIu64 "\n", (uint64_t)index->offset);
 						PPR; printf("          number: %u\n", (unsigned)index->number);
 					}
 				}

src/metaflac/operations_shorthand_streaminfo.c
@@ -75,11 +75,7 @@ FLAC__bool do_shorthand_operation__streaminfo(const char *filename, FLAC__bool p
 			printf("%u\n", block->data.stream_info.bits_per_sample);
 			break;
 		case OP__SHOW_TOTAL_SAMPLES:
-#ifdef _MSC_VER
-			printf("%I64u\n", block->data.stream_info.total_samples);
-#else
-			printf("%llu\n", (unsigned long long)block->data.stream_info.total_samples);
-#endif
+			printf("%" PRIu64 "\n", (uint64_t)block->data.stream_info.total_samples);
 			break;
 		case OP__SET_MD5SUM:
 			memcpy(block->data.stream_info.md5sum, operation->argument.streaminfo_md5.value, 16);

src/plugin_xmms/fileinfo.c
@@ -182,7 +182,7 @@ static void remove_tag(GtkWidget * w, gpointer data)
 {
 	(void)w;
 	(void)data;
-	
+
 	FLAC_plugin__tags_delete_tag(tags_, "TITLE");
 	FLAC_plugin__tags_delete_tag(tags_, "ARTIST");
 	FLAC_plugin__tags_delete_tag(tags_, "ALBUM");
@@ -221,17 +221,13 @@ static void show_file_info(void)
 		label_set_text(flac_blocksize, _("Blocksize: variable\n  min/max: %d/%d"), streaminfo.data.stream_info.min_blocksize, streaminfo.data.stream_info.max_blocksize);
 
 	if (streaminfo.data.stream_info.total_samples)
-		label_set_text(flac_samples, _("Samples: %llu\nLength: %d:%.2d"),
+		label_set_text(flac_samples, _("Samples: %" PRIu64 "\nLength: %d:%.2d"),
 				streaminfo.data.stream_info.total_samples,
 				(int)(streaminfo.data.stream_info.total_samples / streaminfo.data.stream_info.sample_rate / 60),
 				(int)(streaminfo.data.stream_info.total_samples / streaminfo.data.stream_info.sample_rate % 60));
 
 	if(!stat(current_filename, &_stat) && S_ISREG(_stat.st_mode)) {
-#if _FILE_OFFSET_BITS == 64
-		label_set_text(flac_filesize, _("Filesize: %lld B"), _stat.st_size);
-#else
-		label_set_text(flac_filesize, _("Filesize: %ld B"), _stat.st_size);
-#endif
+		label_set_text(flac_filesize, _("Filesize: %zd B"), _stat.st_size);
 		if (streaminfo.data.stream_info.total_samples)
 			label_set_text(flac_bitrate, _("Avg. bitrate: %.1f kb/s\nCompression ratio: %.1f%%"),
 					8.0 * (float)(_stat.st_size) / (1000.0 * (float)streaminfo.data.stream_info.total_samples / (float)streaminfo.data.stream_info.sample_rate),

commit 7ee908403e8be011b8a7dd3bf511408b40312c2e

Fix cuesheet bug introduced in ce8a75134cace056f6c436d54b57bad1a1d93797.
This bug was causing the test_metaflac.sh test to fail after Earl Chew's
patch 19050f74eaa1aa6d609ca065e1a854ada5bb6b4c was supposed to fix an
older bug.

src/metaflac/operations_shorthand_cuesheet.c
@@ -165,7 +165,7 @@ FLAC__bool import_cs_from(const char *filename, FLAC__StreamMetadata **cuesheet,
 		for(track = 0; track < cs->num_tracks; track++) {
 			const FLAC__StreamMetadata_CueSheet_Track *tr = cs->tracks+track;
 			for(index = 0; index < tr->num_indices; index++) {
-				sprintf(spec, "%" PRIu64 "u;", (tr->offset + tr->indices[index].offset));
+				sprintf(spec, "%" PRIu64 ";", (tr->offset + tr->indices[index].offset));
 				local_strcat(seekpoint_specification, spec);
 			}
 		}