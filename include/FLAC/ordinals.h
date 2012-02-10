c506b2f43e2fadd70141322c774bc196c2c65cc3
Erik de Castro Lopo <erikd@mega-nerd.com>
2012-02-10 19:19:11 +1100

    include/FLAC/ordinals.h : Remove CPP hackery.
    This change assumes that a C99 <stdint.h> header is available. For
    compilers where that is not the case, the user should provide a
    minimal replacement header.

--- include/FLAC/ordinals.h
+++ include/FLAC/ordinals.h
@@ -32,35 +32,21 @@
 #ifndef FLAC__ORDINALS_H
 #define FLAC__ORDINALS_H
 
-#if !(defined(_MSC_VER) || defined(__BORLANDC__) || defined(__EMX__))
-#include <inttypes.h>
-#endif
+/* If your compiler does not provide <stdint.h> you should provide a replacement
+ * which hss suitable replacements for the following intX_T and uintX_t types.
+ */
 
-typedef signed char FLAC__int8;
-typedef unsigned char FLAC__uint8;
+#include <stdint.h>
 
-#if defined(_MSC_VER) || defined(__BORLANDC__)
-typedef __int16 FLAC__int16;
-typedef __int32 FLAC__int32;
-typedef __int64 FLAC__int64;
-typedef unsigned __int16 FLAC__uint16;
-typedef unsigned __int32 FLAC__uint32;
-typedef unsigned __int64 FLAC__uint64;
-#elif defined(__EMX__)
-typedef short FLAC__int16;
-typedef long FLAC__int32;
-typedef long long FLAC__int64;
-typedef unsigned short FLAC__uint16;
-typedef unsigned long FLAC__uint32;
-typedef unsigned long long FLAC__uint64;
-#else
+typedef int8_t FLAC__int8;
+typedef uint8_t FLAC__uint8;
+
 typedef int16_t FLAC__int16;
 typedef int32_t FLAC__int32;
 typedef int64_t FLAC__int64;
 typedef uint16_t FLAC__uint16;
 typedef uint32_t FLAC__uint32;
 typedef uint64_t FLAC__uint64;
-#endif
 
 typedef int FLAC__bool;
 
