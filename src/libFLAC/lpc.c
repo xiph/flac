xiph/flac

commit 8bbbf56403808ff75126cd0840a936aedbc4113b

Fix compiler warnings.

src/libFLAC/lpc.c
@@ -112,7 +112,7 @@ void FLAC__lpc_compute_autocorrelation(const FLAC__real data[], unsigned data_le
 void FLAC__lpc_compute_lp_coefficients(const FLAC__real autoc[], unsigned *max_order, FLAC__real lp_coeff[][FLAC__MAX_LPC_ORDER], FLAC__double error[])
 {
 	unsigned i, j;
-	FLAC__double r, err, ref[FLAC__MAX_LPC_ORDER], lpc[FLAC__MAX_LPC_ORDER];
+	FLAC__double r, err, lpc[FLAC__MAX_LPC_ORDER];
 
 	FLAC__ASSERT(0 != max_order);
 	FLAC__ASSERT(0 < *max_order);
@@ -126,7 +126,6 @@ void FLAC__lpc_compute_lp_coefficients(const FLAC__real autoc[], unsigned *max_o
 		r = -autoc[i+1];
 		for(j = 0; j < i; j++)
 			r -= lpc[j] * autoc[i-j];
-		ref[i] = (r/=err);
 
 		/* Update LPC coefficients and total error. */
 		lpc[i]=r;

src/flac/encode.c
@@ -275,7 +275,6 @@ static FLAC__bool get_sample_info_wave(EncoderSession *e, encode_options_t optio
 			FLAC__uint16 x;
 			FLAC__uint32 xx, data_bytes;
 			FLAC__uint16 wFormatTag; /* wFormatTag word from the 'fmt ' chunk */
-			unsigned block_align;
 
 			if(got_fmt_chunk) {
 				flac__utils_printf(stderr, 1, "%s: ERROR: file has multiple 'fmt ' chunks\n", e->inbasefilename);
@@ -368,7 +367,7 @@ static FLAC__bool get_sample_info_wave(EncoderSession *e, encode_options_t optio
 			/* block align */
 			if(!read_uint16(e->fin, /*big_endian=*/false, &x, e->inbasefilename))
 				return false;
-			block_align = (unsigned)x;
+
 			/* bits per sample */
 			if(!read_uint16(e->fin, /*big_endian=*/false, &x, e->inbasefilename))
 				return false;

src/share/replaygain_analysis/replaygain_analysis.c
@@ -147,13 +147,13 @@ static unsigned long    totsamp;
 static double           lsum;
 static double           rsum;
 static int              freqindex;
-#ifndef __sun
+#if 0
 static Uint32_t  A [(size_t)(STEPS_per_dB * MAX_dB)];
 static Uint32_t  B [(size_t)(STEPS_per_dB * MAX_dB)];
 #else
 /* [JEC] Solaris Forte compiler doesn't like float calc in array indices */
-static Uint32_t  A [12000];
-static Uint32_t  B [12000];
+static Uint32_t  A [120 * 100];
+static Uint32_t  B [120 * 100];
 #endif
 
 /* for each filter:

src/test_grabbag/cuesheet/main.c
@@ -111,7 +111,7 @@ static int do_cuesheet(const char *infilename, unsigned sample_rate, FLAC__bool 
 int main(int argc, char *argv[])
 {
 	FLAC__uint64 lead_out_offset;
-	unsigned sample_rate;
+	unsigned sample_rate = 48000;
 	FLAC__bool is_cdda = false;
 	const char *usage = "usage: test_cuesheet cuesheet_file lead_out_offset [ [ sample_rate ] cdda ]\n";
 

commit fc360735ce4d1aa88a94bfccdd3bea5bdd19a8d6

Fix a regression that severely impacted compression levels.
Regression was introducted in commit 8bbbf56403808ff75126cd0840a936aedbc4113b
(Feb 1 19:58:09 2012) when fixing compiler warnings. In src/libFLAC/lpc.c the
line:

        ref[i] = (r/=err);

was erroniously removed because the left hand side, ref[i] was never used.
Obviously, the correct thing to do was to replace that line with:

        r /= err;

This code has not been officially released. The only people who would have
been affected are people who compiled FLAC from git between February and
now.

The only adverse affect of this error was that compression ratio would have
been severely compromised. No audio is lost, and if anyone has a file that
compressed with a bad version of FLAC can decompress it to WAV and then
re-compress with a fixed version.

src/libFLAC/lpc.c
@@ -140,6 +140,7 @@ void FLAC__lpc_compute_lp_coefficients(const FLAC__real autoc[], unsigned *max_o
 		r = -autoc[i+1];
 		for(j = 0; j < i; j++)
 			r -= lpc[j] * autoc[i-j];
+		r /= err;
 
 		/* Update LPC coefficients and total error. */
 		lpc[i]=r;