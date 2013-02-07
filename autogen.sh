d75cb67d7ff40d9e7379791558f804434c06826c
Timothy B. Terriberry <tterribe@xiph.org>
2013-02-07 12:33:03 -0800

    Robustify autogen.sh
    This allows the script to be run from somewhere other than the
     top-level build directory.
    
    Signed-off-by: Erik de Castro Lopo <erikd@mega-nerd.com>

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,13 @@
 # Run this to set up the build system: configure, makefiles, etc.
 # We trust that the user has a recent enough autoconf & automake setup
 # (not older than a few years...)
+set -e
+
+srcdir=`dirname $0`
+test -n "$srcdir" && cd "$srcdir"
+
+echo "Updating build configuration files for FLAC, please wait...."
+
 touch config.rpath
-autoreconf -i
-#$srcdir/configure "$@" && echo
+autoreconf -isf
+#./configure "$@" && echo
