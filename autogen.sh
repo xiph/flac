619b43df36b725e11e12db768d558b1930a583b6
Erik de Castro Lopo <erikd@mega-nerd.com>
2013-03-18 19:35:30 +1100

    autogen.sh : OpenBSD tweaks.

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,14 @@
 # (not older than a few years...)
 set -e
 
+if test $(uname -s) = "OpenBSD" ; then
+	# OpenBSD needs these environment variables set.
+	AUTOCONF_VERSION=2.69
+	AUTOMAKE_VERSION=1.11
+	export AUTOCONF_VERSION
+	export AUTOMAKE_VERSION
+	fi
+
 srcdir=`dirname $0`
 test -n "$srcdir" && cd "$srcdir"
 
