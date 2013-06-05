21a70169f8b8644d7737bf2a8b3f972b38646540
Erik de Castro Lopo <erikd@mega-nerd.com>
2013-06-05 18:48:19 +1000

    XMMS plugin : Fix symbol visibility.
    FLAC now defaults to building with -fvisibility=hidden on GCC4-ELF
    platforms.  This breaks the XMMS plugin.  At least get_iplugin_info()
    needs to be externally visible.
    
    Patch from Christian Weisgerber <naddy@mips.inka.de>

--- src/plugin_xmms/plugin.c
+++ src/plugin_xmms/plugin.c
@@ -140,7 +140,7 @@ static FLAC__bool is_big_endian_host_;
 static unsigned bitrate_history_[BITRATE_HIST_SIZE];
 
 
-InputPlugin *get_iplugin_info(void)
+FLAC_API InputPlugin *get_iplugin_info(void)
 {
 	flac_ip.description = g_strdup_printf("Reference FLAC Player v%s", FLAC__VERSION_STRING);
 	return &flac_ip;
