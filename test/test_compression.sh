39d9791b03adf1883839cae1bd488d9a615bba09
Erik de Castro Lopo <erikd@mega-nerd.com>
2014-11-25 20:02:22 +1100

    More date fixes.

--- test/test_compression.sh
+++ test/test_compression.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 #  FLAC - Free Lossless Audio Codec
-#  Copyright (C) 2012  Xiph.Org Foundation
+#  Copyright (C) 2012-2014  Xiph.Org Foundation
 #
 #  This file is part the FLAC project.  FLAC is comprised of several
 #  components distributed under different licenses.  The codec libraries
