ffcda161ad1f47d730490c91bb95653c4a0be403
Erik de Castro Lopo <erikd@mega-nerd.com>
2015-02-21 08:47:41 +1100

    Add file ci/flac-autotool.sh.
    Currently only does 'make check' because 'make distcheck' is currently
    broken.

---
+++ ci/flac-autotool.sh
@@ -0,0 +1,14 @@
+# Continuous integration build script for FLAC.
+# This script is run by automated frameworks to verify commits
+# see https://mf4.xiph.org/jenkins/job/flac/
+
+# This is intended to be run from the top-level source directory.
+
+set -x
+
+./autogen.sh
+
+./configure
+
+# Should do 'distcheck' here instead of 'check', but 'distcheck' is currently busted.
+V=1 make clean check
