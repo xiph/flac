6ced857c253d5da286bf31adfd74a627aaa43c77
Erik de Castro Lopo <erikd@mega-nerd.com>
2015-07-13 22:15:58 +1000

    libFLAC: Fix decoder_flush with uninitialized decoder
    Closes: https://sourceforge.net/p/flac/bugs/428

--- src/libFLAC/stream_decoder.c
+++ src/libFLAC/stream_decoder.c
@@ -936,6 +936,9 @@ FLAC_API FLAC__bool FLAC__stream_decoder_flush(FLAC__StreamDecoder *decoder)
 	FLAC__ASSERT(0 != decoder->private_);
 	FLAC__ASSERT(0 != decoder->protected_);
 
+	if(!decoder->private_->internal_reset_hack && decoder->protected_->state == FLAC__STREAM_DECODER_UNINITIALIZED)
+		return false;
+
 	decoder->private_->samples_decoded = 0;
 	decoder->private_->do_md5_checking = false;
 
