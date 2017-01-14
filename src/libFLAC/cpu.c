d178058028050aeec649c85644cc9eed08f0aa37
sezero <sezero@users.sourceforge.net>
2017-01-15 01:56:56 +0300

    libFLAC/cpu.c: Replace memory.h include with string.h
    cpu.c was the only source to use memory.h instead of string.h.
    
    Signed-off-by: Erik de Castro Lopo <erikd@mega-nerd.com>

--- src/libFLAC/cpu.c
+++ src/libFLAC/cpu.c
@@ -37,7 +37,7 @@
 #include "private/cpu.h"
 #include "share/compat.h"
 #include <stdlib.h>
-#include <memory.h>
+#include <string.h>
 
 #if defined(_MSC_VER)
 #  include <intrin.h> /* for __cpuid() and _xgetbv() */
