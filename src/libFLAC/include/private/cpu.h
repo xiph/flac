f7cd466c24fb5d1966943f3ea36a1f4a37858597
Erik de Castro Lopo <erikd@mega-nerd.com>
2017-07-19 19:28:30 +1000

    Revert "Add Clang support for FLAC__SSE_TARGET"
    This reverts commit b52fa0ec6dc83ec0f430ff7bd7c80485c45728b0.
    
    See: https://github.com/xiph/flac/commit/b52fa0ec6dc83ec0f430ff7bd7c80485c45728b0

--- src/libFLAC/include/private/cpu.h
+++ src/libFLAC/include/private/cpu.h
@@ -108,8 +108,7 @@
   #if __has_builtin(__builtin_ia32_pabsd256)
     #define FLAC__AVX2_SUPPORTED 1
   #endif
-#elif defined __GNUC__ && ((__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9)) /* GCC 4.9+ */ \
-  || (defined __clang__ && (__clang_major__ > 3) || ( __clang_major__ == 3 || __clang_minor__ >= 8)) /* clang 3.8+ */)
+#elif defined __GNUC__ && !defined __clang__ && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 9)) /* GCC 4.9+ */
   #define FLAC__SSE_TARGET(x) __attribute__ ((__target__ (x)))
   #define FLAC__SSE_SUPPORTED 1
   #define FLAC__SSE2_SUPPORTED 1
