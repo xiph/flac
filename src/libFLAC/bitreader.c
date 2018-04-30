65c2796402077040a1943e93003ed6e5b9317d79
Robert Kausch <robert.kausch@freac.org>
2018-04-30 18:09:51 +0200

    Fix CRC calculation for small blocks.

--- src/libFLAC/bitreader.c
+++ src/libFLAC/bitreader.c
@@ -128,7 +128,7 @@ static inline void crc16_update_word_(FLAC__BitReader *br, brword word)
 
 static inline void crc16_update_block_(FLAC__BitReader *br)
 {
-	if(br->crc16_align)
+	if(br->consumed_words > br->crc16_offset && br->crc16_align)
 		crc16_update_word_(br, br->buffer[br->crc16_offset++]);
 
 #if FLAC__BYTES_PER_WORD == 4
