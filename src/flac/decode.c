edfbe3cf194768af2f4c3cfd5cb5a6a273f63964
lvqcl <lvqcl.mail@gmail.com>
2018-08-25 10:23:39 +0300

    Move CreateFile_utf8 function to a more logical place

--- src/flac/decode.c
+++ src/flac/decode.c
@@ -267,7 +267,7 @@ void DecoderSession_destroy(DecoderSession *d, FLAC__bool error_occurred)
 		if(!error_occurred) {
 			FLAC__off_t written_size = ftello(d->fout);
 			if(written_size > 0) {
-				HANDLE fh = grabbag__CreateFile_utf8(d->outfilename, GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+				HANDLE fh = CreateFile_utf8(d->outfilename, GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 				if(fh != INVALID_HANDLE_VALUE) {
 					if(GetFileType(fh) == FILE_TYPE_DISK) {
 						LARGE_INTEGER size;
@@ -383,7 +383,7 @@ FLAC__bool DecoderSession_process(DecoderSession *d)
 
 #if defined _WIN32 && !defined __CYGWIN__
 	if(!d->analysis_mode && !d->test_only && d->total_samples > 0 && d->fout != stdout) {
-		HANDLE fh = grabbag__CreateFile_utf8(d->outfilename, GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+		HANDLE fh = CreateFile_utf8(d->outfilename, GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 		if(fh != INVALID_HANDLE_VALUE) {
 			if (GetFileType(fh) == FILE_TYPE_DISK) {
 				LARGE_INTEGER size;
