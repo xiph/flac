194c9ba98aa49827dc693026c58c7533ebd67d80
pkubaj <pkubaj@users.noreply.github.com>
2019-08-15 10:38:55 +0000

    Use hwcaps everywhere

--- src/libFLAC/cpu.c
+++ src/libFLAC/cpu.c
@@ -245,21 +245,18 @@ ppc_cpu_info (FLAC__CPUInfo *info)
 #define PPC_FEATURE2_ARCH_2_07		0x80000000
 #endif
 
-#ifdef __linux__
-	if (getauxval(AT_HWCAP2) & PPC_FEATURE2_ARCH_3_00) {
-		info->ppc.arch_3_00 = true;
-	} else if (getauxval(AT_HWCAP2) & PPC_FEATURE2_ARCH_2_07) {
-		info->ppc.arch_2_07 = true;
-	}
-#elif defined(__FreeBSD__)
 	long hwcaps;
+#ifdef __linux__
+	hwcaps = getauxval(AT_HWCAP2);
+#elif defined (__FreeBSD__)
 	elf_aux_info(AT_HWCAP2, &hwcaps, sizeof(hwcaps));
-	if (hwcaps & PPC_FEATURE2_ARCH_3_00) {
+#endif
+	
+	if (hwcaps) & PPC_FEATURE2_ARCH_3_00) {
 		info->ppc.arch_3_00 = true;
 	} else if (hwcaps & PPC_FEATURE2_ARCH_2_07) {
 		info->ppc.arch_2_07 = true;
 	}
-#endif
 #else
 	info->ppc.arch_2_07 = false;
 	info->ppc.arch_3_00 = false;
