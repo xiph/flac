44e51155008238fdc6bb2a32495218b90b91e805
Neelkamal Semwal <neelkamal.semwal@ittiam.com>
2021-07-14 09:43:05 +0530

    libFLAC: Restrict number of bytes in a frame

--- src/libFLAC/stream_decoder.c
+++ src/libFLAC/stream_decoder.c
@@ -2996,7 +2996,7 @@ FLAC__bool seek_to_absolute_sample_(FLAC__StreamDecoder *decoder, FLAC__uint64 s
 	FLAC__uint64 first_frame_offset = decoder->private_->first_frame_offset, lower_bound, upper_bound, lower_bound_sample, upper_bound_sample, this_frame_sample;
 	FLAC__int64 pos = -1;
 	int i;
-	uint32_t approx_bytes_per_frame;
+	FLAC__int64 approx_bytes_per_frame;
 	FLAC__bool first_seek = true;
 	const FLAC__uint64 total_samples = FLAC__stream_decoder_get_total_samples(decoder);
 	const uint32_t min_blocksize = decoder->private_->stream_info.data.stream_info.min_blocksize;
@@ -3042,14 +3042,10 @@ FLAC__bool seek_to_absolute_sample_(FLAC__StreamDecoder *decoder, FLAC__uint64 s
 	upper_bound = stream_length;
 	upper_bound_sample = total_samples > 0 ? total_samples : target_sample /*estimate it*/;
 
-	if(decoder->protected_->state == FLAC__STREAM_DECODER_READ_FRAME) {
-		if(target_sample < decoder->private_->samples_decoded) {
-			if(FLAC__stream_decoder_get_decode_position(decoder, &upper_bound))
-				upper_bound_sample = decoder->private_->samples_decoded;
-		} else {
-			if(FLAC__stream_decoder_get_decode_position(decoder, &lower_bound))
-				lower_bound_sample = decoder->private_->samples_decoded;
-		}
+	/* seeking beyond the end of the stream */
+	if(target_sample > upper_bound_sample) {
+		decoder->protected_->state = FLAC__STREAM_DECODER_SEEK_ERROR;
+		return false;
 	}
 
 	/*
@@ -3179,7 +3175,7 @@ FLAC__bool seek_to_absolute_sample_(FLAC__StreamDecoder *decoder, FLAC__uint64 s
 				return false;
 			}
 			/* our last move backwards wasn't big enough, try again */
-			approx_bytes_per_frame = approx_bytes_per_frame? approx_bytes_per_frame * 2 : 16;
+			approx_bytes_per_frame = (approx_bytes_per_frame > (upper_bound - lower_bound)) ? (upper_bound - lower_bound) : approx_bytes_per_frame ? approx_bytes_per_frame * 2 : 16;
 			continue;
 		}
 		/* allow one seek over upper bound, so we can get a correct upper_bound_sample for streams with unknown total_samples */
