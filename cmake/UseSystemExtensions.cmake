bfab321ac44e38e10d4096dbfd3852bd46ea1662
Rosen Penev <rosenp@gmail.com>
2022-04-17 19:47:14 -0700

    remove completely unused headers
    none of these checks are used by the code.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- cmake/UseSystemExtensions.cmake
+++ cmake/UseSystemExtensions.cmake
@@ -16,37 +16,18 @@ endif()
 check_c_source_compiles("
     #define __EXTENSIONS__ 1
     #include <stdio.h>
-    #ifdef HAVE_SYS_TYPES_H
-    # include <sys/types.h>
-    #endif
-    #ifdef HAVE_SYS_STAT_H
-    # include <sys/stat.h>
-    #endif
     #ifdef STDC_HEADERS
     # include <stdlib.h>
     # include <stddef.h>
-    #else
-    # ifdef HAVE_STDLIB_H
-    #  include <stdlib.h>
-    # endif
     #endif
     #ifdef HAVE_STRING_H
-    # if !defined STDC_HEADERS && defined HAVE_MEMORY_H
-    #  include <memory.h>
-    # endif
     # include <string.h>
-    #endif
-    #ifdef HAVE_STRINGS_H
-    # include <strings.h>
     #endif
     #ifdef HAVE_INTTYPES_H
     # include <inttypes.h>
     #endif
     #ifdef HAVE_STDINT_H
     # include <stdint.h>
-    #endif
-    #ifdef HAVE_UNISTD_H
-    # include <unistd.h>
     #endif
     int main() { return 0; }"
     DODEFINE_EXTENSIONS)
