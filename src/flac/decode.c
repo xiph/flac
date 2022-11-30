xiph/flac

commit 3975b3e6a214ebf86460ec196dbe21dc4dedfb24

Check for ID3v2 tag when using flac -t

src/flac/decode.c
@@ -328,6 +328,23 @@ FLAC__bool DecoderSession_init_decoder(DecoderSession *decoder_session, const ch
 			}
 		}
 	}
+	else if(decoder_session->test_only && strcmp(infilename, "-") != 0) {
+		/* When testing, we can be a little more pedantic, as long
+		 * as we can seek properly */
+		FLAC__byte buffer[3];
+		FILE * f = flac_fopen(infilename, "rb");
+
+		if(fread(buffer, 1, 3, f) < 3) {
+			flac__utils_printf(stderr, 1, "%s: ERROR checking for ID3v2 tag\n", decoder_session->inbasefilename);
+			return false;
+		}
+		if(memcmp(buffer, "ID3", 3) == 0){
+			flac__utils_printf(stderr, 1, "%s: WARNING, ID3v2 tag found. This is non-standard and strongly discouraged\n", decoder_session->inbasefilename);
+			if(decoder_session->treat_warnings_as_errors) {
+				return false;
+			}
+		}
+	}
 
 	decoder_session->decoder = FLAC__stream_decoder_new();
 

commit 8ea5b5112ecac8f88a07190634054217cccc2c74

Fix segfault introduced in commit 3975b3e
A read from the input file was attempted without checking whether
opening it was succesful.

src/flac/decode.c
@@ -332,7 +332,12 @@ FLAC__bool DecoderSession_init_decoder(DecoderSession *decoder_session, const ch
 		/* When testing, we can be a little more pedantic, as long
 		 * as we can seek properly */
 		FLAC__byte buffer[3];
-		FILE * f = flac_fopen(infilename, "rb");
+		FILE * f;
+
+		if(0 == (f = flac_fopen(infilename, "rb"))) {
+			flac__utils_printf(stderr, 1, "ERROR: can't open input file %s: %s\n", infilename, strerror(errno));
+			return false;
+		}
 
 		if(fread(buffer, 1, 3, f) < 3) {
 			flac__utils_printf(stderr, 1, "%s: ERROR checking for ID3v2 tag\n", decoder_session->inbasefilename);