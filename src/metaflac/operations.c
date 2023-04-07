xiph/flac

commit b3b91763304c01eb9e743064feaf91dd9c2d16d6

Add data-format option to metaflac, enabling binary output

src/metaflac/operations.c
@@ -49,6 +49,7 @@ static FLAC__bool do_shorthand_operation__add_padding(const char *filename, FLAC
 
 static FLAC__bool passes_filter(const CommandLineOptions *options, const FLAC__StreamMetadata *block, unsigned block_number);
 static void write_metadata(const char *filename, FLAC__StreamMetadata *block, unsigned block_number, FLAC__bool raw, FLAC__bool hexdump_application);
+static void write_metadata_binary(FLAC__StreamMetadata *block, FLAC__byte *block_raw, FLAC__bool headerless);
 
 /* from operations_shorthand_seektable.c */
 extern FLAC__bool do_shorthand_operation__add_seekpoints(const char *filename, FLAC__Metadata_Chain *chain, const char *specification, FLAC__bool *needs_write);
@@ -189,8 +190,19 @@ FLAC__bool do_major_operation__list(const char *filename, FLAC__Metadata_Chain *
 		ok &= (0 != block);
 		if(!ok)
 			flac_fprintf(stderr, "%s: ERROR: couldn't get block from chain\n", filename);
-		else if(passes_filter(options, FLAC__metadata_iterator_get_block(iterator), block_number))
-			write_metadata(filename, block, block_number, !options->utf8_convert, options->application_data_format_is_hexdump);
+		else if(passes_filter(options, FLAC__metadata_iterator_get_block(iterator), block_number)) {
+			if(!options->data_format_is_binary && !options->data_format_is_binary_headerless)
+				write_metadata(filename, block, block_number, !options->utf8_convert, options->application_data_format_is_hexdump);
+			else {
+				FLAC__byte * block_raw = FLAC__metadata_object_get_raw(block);
+				if(block_raw == 0) {
+					flac_fprintf(stderr, "%s: ERROR: couldn't get block in raw form\n", filename);
+					return false;
+				}
+				write_metadata_binary(block, block_raw, options->data_format_is_binary_headerless);
+				free(block_raw);
+			}
+		}
 		block_number++;
 	} while(ok && FLAC__metadata_iterator_next(iterator));
 
@@ -674,4 +686,14 @@ void write_metadata(const char *filename, FLAC__StreamMetadata *block, unsigned 
 			break;
 	}
 #undef PPR
+}
+
+void write_metadata_binary(FLAC__StreamMetadata *block, FLAC__byte *block_raw, FLAC__bool headerless)
+{
+	if(!headerless)
+		local_fwrite(block_raw, 1, block->length+FLAC__STREAM_METADATA_HEADER_LENGTH, stdout);
+	else if(block->type == FLAC__METADATA_TYPE_APPLICATION && block->length > 3)
+		local_fwrite(block_raw+FLAC__STREAM_METADATA_HEADER_LENGTH+4, 1, block->length-4, stdout);
+	else
+		local_fwrite(block_raw+FLAC__STREAM_METADATA_HEADER_LENGTH, 1, block->length, stdout);
 }

src/metaflac/options.c
@@ -139,6 +139,8 @@ void init_options(CommandLineOptions *options)
 	options->cued_seekpoints = true;
 	options->show_long_help = false;
 	options->show_version = false;
+	options->data_format_is_binary = false;
+	options->data_format_is_binary_headerless = false;
 	options->application_data_format_is_hexdump = false;
 
 	options->ops.operations = 0;
@@ -713,6 +715,8 @@ FLAC__bool parse_option(int option_index, const char *option_argument, CommandLi
 			flac_fprintf(stderr, "ERROR (--%s): illegal data format \"%s\"\n", opt, option_argument);
 			ok = false;
 		}
+		options->data_format_is_binary = arg->value.data_format.is_binary;
+		options->data_format_is_binary_headerless = arg->value.data_format.is_headerless;
 	}
 	else if(0 == strcmp(opt, "application-data-format")) {
 		FLAC__ASSERT(0 != option_argument);
@@ -1108,10 +1112,18 @@ FLAC__bool parse_block_type(const char *in, Argument_BlockType *out)
 
 FLAC__bool parse_data_format(const char *in, Argument_DataFormat *out)
 {
-	if(0 == strcmp(in, "binary"))
-		out->is_binary = true;
-	else if(0 == strcmp(in, "text"))
+	if(0 == strcmp(in, "binary-headerless")) {
 		out->is_binary = false;
+		out->is_headerless = true;
+	}
+	else if(0 == strcmp(in, "binary")) {
+		out->is_binary = true;
+		out->is_headerless = false;
+	}
+	else if(0 == strcmp(in, "text")) {
+		out->is_binary = false;
+		out->is_headerless = false;
+	}
 	else
 		return false;
 	return true;

src/metaflac/options.h
@@ -131,6 +131,7 @@ typedef struct {
 
 typedef struct {
 	FLAC__bool is_binary;
+	FLAC__bool is_headerless;
 } Argument_DataFormat;
 
 typedef struct {
@@ -190,6 +191,8 @@ typedef struct {
 	FLAC__bool cued_seekpoints;
 	FLAC__bool show_long_help;
 	FLAC__bool show_version;
+	FLAC__bool data_format_is_binary;
+	FLAC__bool data_format_is_binary_headerless;
 	FLAC__bool application_data_format_is_hexdump;
 	struct {
 		Operation *operations;

src/metaflac/usage.c
@@ -275,15 +275,16 @@ int long_usage(const char *message, ...)
 	fprintf(out, "    NOTE: if both --block-number and --[except-]block-type are specified,\n");
 	fprintf(out, "          the result is the logical AND of both arguments.\n");
 	fprintf(out, "\n");
-#if 0
-	/*@@@ not implemented yet */
-	fprintf(out, "    --data-format=binary|text\n");
+	fprintf(out, "    --data-format=binary|binary-headerless|text\n");
 	fprintf(out, "    By default a human-readable text representation of the data is displayed.\n");
 	fprintf(out, "    You may specify --data-format=binary to dump the raw binary form of each\n");
-	fprintf(out, "    metadata block.  The output can be read in using a subsequent call to\n");
-	fprintf(out, "    "metaflac --append --from-file=..."\n");
-	fprintf(out, "\n");
+	fprintf(out, "    metadata block. Specify --data-format=binary-headerless to omit output of\n");
+	fprintf(out, "    metadata block headers, including the id of APPLICATION metadata blocks.\n");
+#if 0
+	fprintf(out, "    The output can be read in using a subsequent call\n");
+	fprintf(out, "    to \"metaflac --append --from-file=...\"\n");
 #endif
+	fprintf(out, "\n");
 	fprintf(out, "    --application-data-format=hexdump|text\n");
 	fprintf(out, "    If the application block you are displaying contains binary data but your\n");
 	fprintf(out, "    --data-format=text, you can display a hex dump of the application data\n");

commit ddeeb7b416ab1d35ff90eb61ed07aa493347bc7a

Fix leak in metaflac
This leak seems to have been introduced by b3b9176

Credit: Oss-Fuzz
Issue: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=57641

src/metaflac/operations.c
@@ -197,6 +197,7 @@ FLAC__bool do_major_operation__list(const char *filename, FLAC__Metadata_Chain *
 				FLAC__byte * block_raw = FLAC__metadata_object_get_raw(block);
 				if(block_raw == 0) {
 					flac_fprintf(stderr, "%s: ERROR: couldn't get block in raw form\n", filename);
+					FLAC__metadata_iterator_delete(iterator);
 					return false;
 				}
 				write_metadata_binary(block, block_raw, options->data_format_is_binary_headerless);