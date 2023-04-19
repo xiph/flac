5358db8499de092b1414146d90db7740db0b8455
Martijn van Beurden <mvanb1@gmail.com>
2023-04-19 11:38:00 +0200

    Fix division by zero when handling sample rate == 0
    Credit: Oss-Fuzz
    Issue: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=58142

--- src/flac/encode.c
+++ src/flac/encode.c
@@ -1951,8 +1951,12 @@ FLAC__bool EncoderSession_init_encoder(EncoderSession *e, encode_options_t optio
 			flac_decoder_data->num_metadata_blocks = j;
 			if(options.padding > 0)
 				p = options.padding;
-			if(p < 0)
-				p = e->total_samples_to_encode / sample_rate < 20*60? FLAC_ENCODE__DEFAULT_PADDING : FLAC_ENCODE__DEFAULT_PADDING*8;
+			if(p < 0) {
+				if(sample_rate == 0)
+					p = FLAC_ENCODE__DEFAULT_PADDING;
+				else
+					p = e->total_samples_to_encode / sample_rate < 20*60? FLAC_ENCODE__DEFAULT_PADDING : FLAC_ENCODE__DEFAULT_PADDING*8;
+			}
 			if(p > 0)
 				p += (e->replay_gain ? GRABBAG__REPLAYGAIN_MAX_TAG_SPACE_REQUIRED : 0);
 			p = min(p, (int)((1u << FLAC__STREAM_METADATA_LENGTH_LEN) - 1));
@@ -2015,7 +2019,10 @@ FLAC__bool EncoderSession_init_encoder(EncoderSession *e, encode_options_t optio
 		if(options.padding != 0) {
 			padding.is_last = false; /* the encoder will set this for us */
 			padding.type = FLAC__METADATA_TYPE_PADDING;
-			padding.length = (uint32_t)(options.padding>0? options.padding : (e->total_samples_to_encode / sample_rate < 20*60? FLAC_ENCODE__DEFAULT_PADDING : FLAC_ENCODE__DEFAULT_PADDING*8)) + (e->replay_gain ? GRABBAG__REPLAYGAIN_MAX_TAG_SPACE_REQUIRED : 0);
+			if(sample_rate == 0)
+				padding.length = (uint32_t)(options.padding>0? options.padding : FLAC_ENCODE__DEFAULT_PADDING) + (e->replay_gain ? GRABBAG__REPLAYGAIN_MAX_TAG_SPACE_REQUIRED : 0);
+			else
+				padding.length = (uint32_t)(options.padding>0? options.padding : (e->total_samples_to_encode / sample_rate < 20*60? FLAC_ENCODE__DEFAULT_PADDING : FLAC_ENCODE__DEFAULT_PADDING*8)) + (e->replay_gain ? GRABBAG__REPLAYGAIN_MAX_TAG_SPACE_REQUIRED : 0);
 			padding.length = min(padding.length, (1u << FLAC__STREAM_METADATA_LENGTH_LEN) - 1);
 			static_metadata_append(&static_metadata, &padding, /*needs_delete=*/false);
 		}
@@ -2180,14 +2187,13 @@ FLAC__bool convert_to_seek_table_template(const char *requested_seek_points, int
 	if(num_requested_seek_points == 0 && 0 == cuesheet)
 		return true;
 
-	if(num_requested_seek_points < 0) {
 #if FLAC__HAS_OGG
-		/*@@@@@@ workaround ogg bug: too many seekpoints makes table not fit in one page */
-		if(e->use_ogg && e->total_samples_to_encode > 0 && e->total_samples_to_encode / e->info.sample_rate / 10 > 230)
-			requested_seek_points = "230x;";
-		else
+	if(e->use_ogg)
+		return true;
 #endif
-			requested_seek_points = "10s;";
+
+	if(num_requested_seek_points < 0) {
+		requested_seek_points = "10s;";
 		num_requested_seek_points = 1;
 	}
 
