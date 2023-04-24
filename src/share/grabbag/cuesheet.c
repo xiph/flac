652203715bded01cf8f09cf9e3874c93efb1e304
Martijn van Beurden <mvanb1@gmail.com>
2023-04-24 10:53:44 +0200

    Fix mistakes in 29d1460 and simplify
    Return false was used instead of return -1. Also, overflow is
    checked with signed int, which triggers the undefined behaviour
    sanitizer. Instead, be a little more strict, such large values
    aren't useful in this context anyway.
    
    Credit: Oss-Fuzz
    Issue: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=58268

--- src/share/grabbag/cuesheet.c
+++ src/share/grabbag/cuesheet.c
@@ -55,14 +55,8 @@ static FLAC__int64 local__parse_int64_(const char *s)
 
 	while('\0' != (c = *s++))
 		if(c >= '0' && c <= '9') {
-			if(ret > (INT64_MAX / 10))
-				return false;
-			else if(ret == (INT64_MAX/10)) {
-				FLAC__int64 tmp = ret;
-				ret = ret * 10 + (c - '0');
-				if(ret < tmp)
-					return -1;
-			}
+			if(ret >= (INT64_MAX / 10))
+				return -1;
 			else
 				ret = ret * 10 + (c - '0');
 		}
@@ -97,14 +91,8 @@ static FLAC__int64 local__parse_msf_(const char *s, uint32_t sample_rate)
 		return -1;
 	while(':' != (c = *s++)) {
 		if(c >= '0' && c <= '9') {
-			if(field > (INT64_MAX / 10))
-				return false;
-			else if(field == (INT64_MAX/10)) {
-				FLAC__int64 tmp = field;
-				field = field * 10 + (c - '0');
-				if(field < tmp)
-					return -1;
-			}
+			if(field >= (INT64_MAX / 10))
+				return -1;
 			else
 				field = field * 10 + (c - '0');
 		}
@@ -112,7 +100,7 @@ static FLAC__int64 local__parse_msf_(const char *s, uint32_t sample_rate)
 			return -1;
 	}
 
-	if(field > INT64_MAX / (60 * sample_rate))
+	if(field >= INT64_MAX / (60 * sample_rate))
 		return -1;
 	ret = field * 60 * sample_rate;
 
@@ -189,14 +177,8 @@ static FLAC__int64 local__parse_ms_(const char *s, uint32_t sample_rate)
 		return -1;
 	while(':' != (c = *s++)) {
 		if(c >= '0' && c <= '9') {
-			if(field > (INT64_MAX / 10))
-				return false;
-			else if(field == (INT64_MAX/10)) {
-				FLAC__int64 tmp = field;
-				field = field * 10 + (c - '0');
-				if(field < tmp)
-					return -1;
-			}
+			if(field >= (INT64_MAX / 10))
+				return -1;
 			else
 				field = field * 10 + (c - '0');
 		}
