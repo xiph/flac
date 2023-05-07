ebc2a696973f319c045b9bab87a69b84af7fce9e
Martijn van Beurden <mvanb1@gmail.com>
2023-05-07 15:44:12 +0200

    Fix mistake in eba4b6f and add check to seektable_is_legal
    Credit: Oss-Fuzz
    Issue: https://bugs.chromium.org/p/oss-fuzz/issues/detail?id=58546

--- src/libFLAC/metadata_object.c
+++ src/libFLAC/metadata_object.c
@@ -931,7 +931,7 @@ FLAC_API FLAC__bool FLAC__metadata_object_seektable_resize_points(FLAC__StreamMe
 	FLAC__ASSERT(object != NULL);
 	FLAC__ASSERT(object->type == FLAC__METADATA_TYPE_SEEKTABLE);
 
-	if(new_num_points > ((2 << FLAC__STREAM_METADATA_LENGTH_LEN ) / FLAC__STREAM_METADATA_SEEKPOINT_LENGTH))
+	if((FLAC__uint64)(new_num_points) * FLAC__STREAM_METADATA_SEEKPOINT_LENGTH >= (1u << FLAC__STREAM_METADATA_LENGTH_LEN))
 		return false;
 
 	if (object->data.seek_table.points == 0) {
