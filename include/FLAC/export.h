526f3518f41c9667dfbde581116c612e60fc35cf
Martijn van Beurden <mvanb1@gmail.com>
2023-08-07 14:30:12 +0200

    Update API version numbers and ABI dumps

--- include/FLAC/export.h
+++ include/FLAC/export.h
@@ -95,9 +95,9 @@
 /** These \#defines will mirror the libtool-based library version number, see
  * http://www.gnu.org/software/libtool/manual/libtool.html#Libtool-versioning
  */
-#define FLAC_API_VERSION_CURRENT 13
+#define FLAC_API_VERSION_CURRENT 14
 #define FLAC_API_VERSION_REVISION 0 /**< see above */
-#define FLAC_API_VERSION_AGE 1 /**< see above */
+#define FLAC_API_VERSION_AGE 2 /**< see above */
 
 #ifdef __cplusplus
 extern "C" {
