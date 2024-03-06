9504c76970c59216d5c062b882955bda36238495
Martijn van Beurden <mvanb1@gmail.com>
2024-03-06 14:04:45 +0100

    Handle missing decoder in FLAC__stream_encoder_get_verify_decoder_state()

--- src/libFLAC/stream_encoder.c
+++ src/libFLAC/stream_encoder.c
@@ -2325,7 +2325,10 @@ FLAC_API FLAC__StreamDecoderState FLAC__stream_encoder_get_verify_decoder_state(
 	FLAC__ASSERT(0 != encoder->private_);
 	FLAC__ASSERT(0 != encoder->protected_);
 	if(encoder->protected_->verify)
-		return FLAC__stream_decoder_get_state(encoder->private_->verify.decoder);
+		if(encoder->private_->verify.decoder == NULL)
+			return FLAC__STREAM_DECODER_MEMORY_ALLOCATION_ERROR;
+		else
+			return FLAC__stream_decoder_get_state(encoder->private_->verify.decoder);
 	else
 		return FLAC__STREAM_DECODER_UNINITIALIZED;
 }
