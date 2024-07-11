de8bbb0d5bab1c88d48e163a4ae512395b193bad
Martijn van Beurden <mvanb1@gmail.com>
2024-07-11 15:35:32 +0200

    Update copyright to 2024 (#722)

--- oss-fuzz/tool_metaflac.c
+++ oss-fuzz/tool_metaflac.c
@@ -1,5 +1,5 @@
 /* fuzzer_tool_flac
- * Copyright (C) 2023  Xiph.Org Foundation
+ * Copyright (C) 2023-2024  Xiph.Org Foundation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
