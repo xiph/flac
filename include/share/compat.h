a8adc6c0443144fecf9ff90c954e62eee4ce369c
Martijn van Beurden <mvanb1@gmail.com>
2024-12-27 12:03:31 +0100

    Check for overflow in off_t when parsing chunks (#781)
    Credit: Oss-Fuzz
    Issue: https://issues.oss-fuzz.com/issues/42537590

--- include/share/compat.h
+++ include/share/compat.h
@@ -70,7 +70,7 @@
 #define FLAC__OFF_T_MAX LONG_MAX
 #else
 #define FLAC__off_t off_t
-#define FLAC__OFF_T_MAX OFF_T_MAX
+#define FLAC__OFF_T_MAX (sizeof(off_t) == sizeof(int64_t) ? INT64_MAX : sizeof(off_t) == sizeof(int32_t) ? INT32_MAX : -999999)
 #endif
 #endif
 
